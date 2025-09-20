e588fca9e6cf15863dc0b5bc8bafd6783af96a4a
Akhil <kas032025d@gmail.com>
2025-09-20 16:33:12 +0530

    feat: add contact component with form and styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,10 @@ import { Routes } from '@angular/router';
 // import { DashboardComponent } from './component/dashboard';
 import { Dashboard } from './component/dashboard/dashboard';
 import { About } from './component/about/about';
+import { Contact } from './component/contact/contact';
 
 export const routes: Routes = [
+    {path:'contact',component:Contact},
    {path:'about',component:About},
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: Dashboard },
---
+++ src/app/component/contact/contact.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  imports: [],
+  templateUrl: './contact.html',
+  styleUrl: './contact.css'
+})
+export class Contact {
+        //   document.addEventListener('DOMContentLoaded', function() {
+        //     const contactForm = document.getElementById('contactForm');
+
+        //     contactForm.addEventListener('submit', function(event) {
+        //         event.preventDefault();
+
+
+        //         const name = document.getElementById('name').value;
+        //         const email = document.getElementById('email').value;
+        //         const help = document.getElementById('help').value;
+
+        //         if (name && email && help) {
+
+        //             alert('Thank you for your message! We will get back to you soon.');
+        //             contactForm.reset();
+        //         } else {
+        //             alert('Please fill in all fields.');
+        //         }
+        //     });
+        // });
+
+
+}
